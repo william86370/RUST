1bbbdac77e6c6cd6d044ea562aa614c3da5c649c
Will W <william86370@gmail.com>
2021-10-02 19:36:31 -0400

    added init files

---
+++ Dockerfile
@@ -0,0 +1,61 @@
+###########################################################
+# Dockerfile that unfortantly builds a rust server for K8
+###########################################################
+FROM cm2network/steamcmd:root
+
+LABEL maintainer="william86370@gmail.com"
+
+ENV STEAMAPPID 258550
+ENV STEAMAPP rust
+ENV STEAMAPPDIR "${HOMEDIR}/${STEAMAPP}-dedicated"
+ENV DLURL https://raw.githubusercontent.com/william86370/RUST
+
+# Add entry script & RUST config
+# Remove packages and tidy up
+RUN set -x \
+	&& apt-get update \
+	&& apt-get install -y --no-install-recommends --no-install-suggests \
+		wget=1.20.1-1.1 \
+		ca-certificates \
+		lib32z1=1:1.2.11.dfsg-1 \
+	&& mkdir -p "${STEAMAPPDIR}" \
+	&& wget --max-redirect=30 "${DLURL}/master/etc/entry.sh" -O "${HOMEDIR}/entry.sh" \
+	&& { \
+		echo '@ShutdownOnFailedCommand 1'; \
+		echo '@NoPromptForPassword 1'; \
+		echo 'login anonymous'; \
+		echo 'force_install_dir '"${STEAMAPPDIR}"''; \
+		echo 'app_update '"${STEAMAPPID}"''; \
+		echo 'quit'; \
+	   } > "${HOMEDIR}/${STEAMAPP}_update.txt" \
+	&& chmod +x "${HOMEDIR}/entry.sh" \
+	&& chown -R "${USER}:${USER}" "${HOMEDIR}/entry.sh" "${STEAMAPPDIR}" "${HOMEDIR}/${STEAMAPP}_update.txt" \	
+	&& rm -rf /var/lib/apt/lists/* 
+
+ENV APP_LISENIP=0.0.0.0  \
+APP_PORT=28082  \
+SERVER_IP=0.0.0.0  \
+SERVER_PORT=28015  \
+SERVER_TICKRATE=30  \
+SERVER_HOSTNAME=Rust  \
+SERVER_IDENTITY=rustserver  \
+SERVER_GAMEMODE=vanilla  \
+SERVER_LEVEL="Procedural Map"  \
+SERVER_SEED=0  \
+SERVER_SALT=0  \
+SERVER_MAXPLAYERS=50  \
+SERVER_WORLDSIZE=3000  \
+SERVER_SAVEINTERVAL=300  \
+RCON_WEB=1  \
+RCON_IP=0.0.0.0  \
+RCON_PORT=28016  \
+RCON_PASSWORD="CHANGE_ME" 
+
+
+USER ${USER}
+
+VOLUME ${STEAMAPPDIR}
+
+WORKDIR ${HOMEDIR}
+
+CMD ["bash", "entry.sh"]
---
+++ etc/entry.sh
@@ -0,0 +1,29 @@
+#!/bin/bash 
+mkdir -p "${STEAMAPPDIR}" || true  
+#install rust dedicated server 
+bash "${STEAMCMDDIR}/steamcmd.sh" +login anonymous \
+				+force_install_dir "${STEAMAPPDIR}" \
+				+app_update "${STEAMAPPID}" \
+				+quit
+
+# Believe it or not, if you don't do this rust shits itself
+cd "${STEAMAPPDIR}"
+#Assume all enviroment vars are being passed into the container
+bash "${STEAMAPPDIR}/RustDedicated" -batchmode  +app.listenip ${APP_LISENIP} \
+                                                +app.port ${APP_PORT} \
+                                                +server.ip ${SERVER_IP} \
+                                                +server.port ${SERVER_PORT} \
+                                                +server.tickrate ${SERVER_TICKRATE} \
+                                                +server.hostname "${SERVER_HOSTNAME}" \
+                                                +server.identity "${SERVER_IDENTITY}" \
+                                                +server.gamemode ${SERVER_GAMEMODE} \
+                                                +server.level "${SERVER_LEVEL}" \
+                                                +server.seed ${SERVER_SEED} \
+                                                +server.salt ${SERVER_SALT} \
+                                                +server.maxplayers ${SERVER_MAXPLAYERS} \
+                                                +server.worldsize ${SERVER_WORLDSIZE} \
+                                                +server.saveinterval ${SERVER_SAVEINTERVAL}\
+                                                +rcon.web ${RCON_WEB} \
+                                                +rcon.ip ${RCON_IP} \
+                                                +rcon.port ${RCON_PORT} \
+                                                +rcon.password "${RCON_PASSWORD}"
